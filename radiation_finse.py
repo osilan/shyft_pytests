c3e2b2a5be0f536124ef9ec5b5ee70217a487653
osilan <olga.silantyeva@geo.uio.no>
2019-02-11 13:49:23 +0100

    finse data october first try, need to use another month with positive temperature

--- radiation_finse.py
+++ radiation_finse.py
@@ -165,7 +165,7 @@ with open('csv_finse_initial.csv', mode='r') as csv_file:
 
 # processing data: I want to get only values for measurements between 2016-10-01 to 2016-10-22
 idxstart=timestr.index('2016-10-01 00:00:00')
-idxend=timestr.index('2016-10-22 00:00:00')
+idxend=timestr.index('2016-10-23 00:00:00')
 idxstep=2
 times=timearr[idxstart:idxend:idxstep]
 timespy=timepyobj[idxstart:idxend:idxstep]
@@ -179,11 +179,12 @@ lw_up_oct16=lw_up[idxstart:idxend:idxstep]
 air_temp_2_oct16=air_temp_2[idxstart:idxend:idxstep]
 rh_2_oct16=rh_2[idxstart:idxend:idxstep]
 atmp_oct16=atmp[idxstart:idxend:idxstep]
+print(len(times))
 
 fig, ax1 = plt.subplots(figsize=(7,5))
 fig.suptitle('Finse, Norway, measured data October 2016')
 myFmt=mdates.DateFormatter('%Y-%M-%D %H:%M:%S')
-ax1.plot(timespy,sw_down_oct16, 'r--', label='sw-downwell')
+ax1.plot(timespy,sw_net_oct16, 'r--', label='sw-net')
 ax1.set_ylabel('Rsm, [W/m^2]')
 ax1.set_xlabel('Date-time')
 plt.gcf().autofmt_xdate()
@@ -249,131 +250,140 @@ omega2 = 0
 rsm = 0
 rahrad = 0.0
 
-# while (i<n):
-#     netrad = 0.0
-#     swrad1 = 0.0
-#     swrad3 = 0.0
-#     rarad = 0.0
-#     rarad1 = 0.0
-#     rarad3 = 0.0
-#     ratheor_int = 0.0
-#     j = 1
-#     # rsm = rahrad / 23
-#     rsm = 0.0
-#     rahrad = 0.0
-#     rahrad1 = 0.0
-#     dd = 1 + 0.033 * math.cos(dayi * 2 * math.pi / 365)
-#     G = 2 * math.pi / 365 * (dayi - 1)
-#     declin = 0.006918 - 0.399912 * math.cos(G) + 0.070257 * math.sin(G) - 0.006758 * math.cos(
-#         2 * G) + 0.000907 * math.sin(2 * G) - 0.002697 * math.cos(3 * G) + 0.00148 * math.sin(3 * G)
-#     # tmpra = api.DoubleVector()
-#     while (j<24):
-#         time1 = ta.time(i*24+j-1)
-#         time2 = ta.time(i * 24 + j)
-#         # print(time1)
-#         # print(time2)
-#         if ((3*j)<24):
-#             time0 = ta.time(i * 24 + j*3-3)
-#             # time3 = ta.time(i * 24 + j * 3 )
-#             if (j>7):
-#                 time3 = ta.time(i*24+j*3-1)
-#             else:
-#                 time3 = ta.time(i*24+j*3)
-#             # print(time0)
-#             # print(time3)
-#             radcaly3.net_radiation_step(radresy3, latitude, time0, time3, slope_deg, aspect_deg, tempP1, rhP1, elevation, rsm)
-#             swrad3 += radresy3.sw_radiation
-#             rarad3 += radresy3.ra
-#         # print(time1)
-#         # print(time)
-#         # print("--------")
-#         radcaly.net_radiation(radresy, latitude, time1, slope_deg, aspect_deg, tempP1, rhP1, elevation,rsm)
-#         radcaly1.net_radiation_step(radresy1, latitude, time1, time2, slope_deg, aspect_deg, tempP1, rhP1, elevation,rsm)
-#
-#         # omega = 15*(j-12)*math.pi/180
-#         omega1 = radresy.sun_rise * math.pi / 180
-#         omega2 = radresy.sun_set * math.pi / 180
-#         # print(omega)
-#         # print("omega1:", omega1)
-#         netrad += radresy.sw_radiation
-#         swrad1 += radresy1.sw_radiation
-#
-#         rarad += radresy.ra
-#         rahrad += radresy.rah
-#         rarad1 += radresy1.ra
-#         rahrad1 += radresy1.rah
-#         j+=1
-#     net_rad.append(netrad/23)
-#     swcalc_step1.append(swrad1)
-#     # print("--- 1-h step ---")
-#     # print("swrad1: ",swrad1)
-#     ra_rad.append(rarad/23)
-#     ra_rad1.append(rarad1)
-#     # print("rarad1: ", rarad1)
-#     # radtheorint_arr.append(ratheor_int/23)
-#     # print("--- 3-h step ---")
-#     swcalc_step3.append(swrad3)
-#     # print("swrad3: ", swrad3)
-#     radcalc_step3.append(rarad3)
-#     # print("rarad3: ", rarad3)
-#     # print(dayi)
-#     # print("-----------")
-#     time1 = ta.time(i*24)
-#     time = ta.time(i*24+23)
-#     # print("--- 24-h step ---")
-#     # print(time1)
-#     # print(time)
-#     radcaly24.net_radiation_step(radresy24, latitude, time1, time, slope_deg, aspect_deg, tempP1, rhP1, elevation, rsm)
-#     # print("swstep: ", radresy24.sw_radiation)
-#     swcalc_step24.append(radresy24.sw_radiation)
-#     radcalc_step24.append(radresy24.ra)
-#     # print("-----------")
-#     # print(ratheor_int/23)
-#     # print(rarad/23)
-#
-#     # print(radresy.omega1)
-#     # print(radresy.omega2)
-#
-#     declin_arr.append(declin*180/math.pi)
-#     # print("omega1: ", omega1)
-#     # print("omega2: ", omega2)
-#     ra_theor = gsc*dd*(math.sin(declin)*math.sin(lat_rad)*math.cos(slope)*(omega2-omega1)-math.sin(declin)*math.cos(lat_rad)*math.sin(slope)*math.cos(aspect)*(omega2-omega1)+math.cos(declin)*math.cos(lat_rad)*math.cos(slope)*(math.sin(omega2)-math.sin(omega1))+math.cos(declin)*math.sin(lat_rad)*math.sin(slope)*math.cos(aspect)*(math.sin(omega2)-math.sin(omega1))-math.cos(declin)*math.sin(slope)*math.sin(aspect)*(math.cos(omega2)-math.cos(omega1)))
-#     # ra_theor =  gsc * dd * (
-#     #             math.sin(declin) * math.sin(lat) * math.cos(slope) * (omega2 - omega1)  + math.cos(declin) * math.cos(lat) * math.cos(
-#     #         slope) * (math.sin(omega2) - math.sin(omega1)))
-#     # print(ra_theor/math.pi/2)
-#     # print(dd)
-#     rat_rad.append(ra_theor/math.pi/2)
-#     rah_rad.append(rahrad / 23)
-#     j = 1
-#     i+=1
-#     dayi += 1
-#     doy.append(dayi)
-#
-#
-# # Let's plot the data we received from HbvSnow
-# fig, ax1 = plt.subplots(figsize=(7,5))
-# # ax2 = ax1.twinx()
-# # ax1.plot(doy, rat_rad, 'g.-', label='Ratheor-integral')
+sw_simulated_1h = api.DoubleVector()
+
+while (i<n):
+    netrad = 0.0
+    swrad1 = 0.0
+    swrad3 = 0.0
+    rarad = 0.0
+    rarad1 = 0.0
+    rarad3 = 0.0
+    ratheor_int = 0.0
+    j = 1
+    # rsm = rahrad / 23
+    rsm = 0.0
+    rahrad = 0.0
+    rahrad1 = 0.0
+    dd = 1 + 0.033 * math.cos(dayi * 2 * math.pi / 365)
+    G = 2 * math.pi / 365 * (dayi - 1)
+    declin = 0.006918 - 0.399912 * math.cos(G) + 0.070257 * math.sin(G) - 0.006758 * math.cos(
+        2 * G) + 0.000907 * math.sin(2 * G) - 0.002697 * math.cos(3 * G) + 0.00148 * math.sin(3 * G)
+    # tmpra = api.DoubleVector()
+    while (j<24):
+        time1 = ta.time(i*24+j-1)
+        time2 = ta.time(i * 24 + j)
+        tempP1= air_temp_2_oct16[i * 24 + j]
+        print("temperature: ", tempP1)
+        rhP1 = rh_2_oct16[i*24+j]
+        print("rhumidity: ",rhP1)
+        rsm = sw_net_oct16[i*24+j]
+        print("measured Rs: ", rsm)
+        # print(time1)
+        # print(time2)
+        # if ((3*j)<24):
+        #     time0 = ta.time(i * 24 + j*3-3)
+        #     # time3 = ta.time(i * 24 + j * 3 )
+        #     if (j>7):
+        #         time3 = ta.time(i*24+j*3-1)
+        #     else:
+        #         time3 = ta.time(i*24+j*3)
+        #     # print(time0)
+        #     # print(time3)
+        #     radcaly3.net_radiation_step(radresy3, latitude, time0, time3, slope_deg, aspect_deg, tempP1, rhP1, elevation, rsm)
+        #     swrad3 += radresy3.sw_radiation
+        #     rarad3 += radresy3.ra
+        # print(time1)
+        # print(time)
+        # print("--------")
+        radcaly.net_radiation(radresy, latitude, time1, slope_deg, aspect_deg,tempP1, rhP1, elevation,rsm)
+        radcaly1.net_radiation_step(radresy1, latitude, time1, time2, slope_deg, aspect_deg, tempP1, rhP1, elevation,rsm)
+
+        # omega = 15*(j-12)*math.pi/180
+        omega1 = radresy.sun_rise * math.pi / 180
+        omega2 = radresy.sun_set * math.pi / 180
+        # print(omega)
+        # print("omega1:", omega1)
+        netrad += radresy.sw_radiation
+        swrad1 += radresy1.sw_radiation
+        sw_simulated_1h.append(radresy.sw_radiation)
+
+        rarad += radresy.ra
+        rahrad += radresy.rah
+        rarad1 += radresy1.ra
+        rahrad1 += radresy1.rah
+        j+=1
+    net_rad.append(netrad/23)
+    swcalc_step1.append(swrad1)
+    # print("--- 1-h step ---")
+    # print("swrad1: ",swrad1)
+    ra_rad.append(rarad/23)
+    ra_rad1.append(rarad1)
+    # print("rarad1: ", rarad1)
+    # radtheorint_arr.append(ratheor_int/23)
+    # print("--- 3-h step ---")
+    swcalc_step3.append(swrad3)
+    # print("swrad3: ", swrad3)
+    radcalc_step3.append(rarad3)
+    # print("rarad3: ", rarad3)
+    # print(dayi)
+    # print("-----------")
+    time1 = ta.time(i*24)
+    time = ta.time(i*24+23)
+    # print("--- 24-h step ---")
+    # print(time1)
+    # print(time)
+    radcaly24.net_radiation_step(radresy24, latitude, time1, time, slope_deg, aspect_deg, tempP1, rhP1, elevation, rsm)
+    # print("swstep: ", radresy24.sw_radiation)
+    swcalc_step24.append(radresy24.sw_radiation)
+    radcalc_step24.append(radresy24.ra)
+    # print("-----------")
+    # print(ratheor_int/23)
+    # print(rarad/23)
+
+    # print(radresy.omega1)
+    # print(radresy.omega2)
+
+    declin_arr.append(declin*180/math.pi)
+    # print("omega1: ", omega1)
+    # print("omega2: ", omega2)
+    ra_theor = gsc*dd*(math.sin(declin)*math.sin(lat_rad)*math.cos(slope)*(omega2-omega1)-math.sin(declin)*math.cos(lat_rad)*math.sin(slope)*math.cos(aspect)*(omega2-omega1)+math.cos(declin)*math.cos(lat_rad)*math.cos(slope)*(math.sin(omega2)-math.sin(omega1))+math.cos(declin)*math.sin(lat_rad)*math.sin(slope)*math.cos(aspect)*(math.sin(omega2)-math.sin(omega1))-math.cos(declin)*math.sin(slope)*math.sin(aspect)*(math.cos(omega2)-math.cos(omega1)))
+    # ra_theor =  gsc * dd * (
+    #             math.sin(declin) * math.sin(lat) * math.cos(slope) * (omega2 - omega1)  + math.cos(declin) * math.cos(lat) * math.cos(
+    #         slope) * (math.sin(omega2) - math.sin(omega1)))
+    # print(ra_theor/math.pi/2)
+    # print(dd)
+    rat_rad.append(ra_theor/math.pi/2)
+    rah_rad.append(rahrad / 23)
+    j = 1
+    i+=1
+    dayi += 1
+    doy.append(dayi)
+
+
+# Let's plot the data we received from HbvSnow
+fig, ax1 = plt.subplots(figsize=(7,5))
+# ax2 = ax1.twinx()
+# ax1.plot(doy, rat_rad, 'g.-', label='Ratheor-integral')
 # ax1.plot(doy, ra_rad, 'r--', label='Ra-instant')
 # ax1.plot(doy, net_rad, 'r', label='Rso-instant')
-# # ax1.plot(doy, radtheorint_arr, 'y', label='Rso')
+# ax1.plot(doy, radtheorint_arr, 'y', label='Rso')
 # ax1.plot(doy, ra_rad1, 'k--', label='Ra-1h-step')
 # ax1.plot(doy, swcalc_step1, 'k', label='Rso-1h-step')
-# ax1.plot(doy, radcalc_step3, 'b--', label='Ra-3h-step')
-# ax1.plot(doy, swcalc_step3, 'b', label='Rso-3h-step')
 # ax1.plot(doy, radcalc_step24, 'y-.', label='Ra-24h-step')
 # ax1.plot(doy, swcalc_step24, 'y', label='Rso-24h-step')
-#
-# ax1.set_ylabel('Ra, Rso, [W/m^2]')
-# # ax2.set_ylabel('extraterrestrial radiation (Ra), [W/m^2]')
-# ax1.set_xlabel('DOY')
-# plt.title("Golden, CO, surface slope 90 N")
-# plt.legend(loc="upper left")
-# # plt.legend(loc="lower center")
-# plt.axis([0,365,0,120])
-# plt.grid(True)
-# plt.show()
+ax1.plot(sw_net_oct16[0:506], 'm--', label='Rsm net')
+ax1.plot(sw_simulated_1h, 'm', label='SW simulated net')
+# plt.gcf().autofmt_xdate()
+ax1.set_ylabel('Ra, Rso, [W/m^2]')
+# ax2.set_ylabel('extraterrestrial radiation (Ra), [W/m^2]')
+ax1.set_xlabel('Day-time')
+plt.title("Golden, CO, surface slope 90 N")
+plt.legend(loc="upper left")
+# plt.legend(loc="lower center")
+plt.axis([0,22,-100,500])
+plt.grid(True)
+plt.show()
 
 
 print(" --------------------------------------------------------- ")
