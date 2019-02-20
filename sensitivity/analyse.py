4f07cf0f7ee2e110e92c51674588b1870132740c
osilan <olga.silantyeva@geo.uio.no>
2019-02-20 13:52:11 +0100

    albedo,turbidity, elevation, asoect dependency of radiation; run_pm initial commit

--- sensitivity/analyse.py
+++ sensitivity/analyse.py
@@ -108,23 +108,122 @@ gsc = 1367
 #     i+=1
 # plt.show()
 
-elevation_array = [-100.0, -20.0,0.0, 20.0, 100.0,400.0, 600.0, 800.0, 1000.0, 1800.0]
-slope_deg = 90.0
-ymax = 450
+# elevation_array = [-100.0, -20.0,0.0, 20.0, 100.0,400.0, 600.0, 800.0, 1000.0, 1800.0, 5000.0, 8800.0]
+# slope_deg = 90.0
+# ymax = 350
+# ymin = -20
+# yname = 'Rso, [W/m^2]'
+# xname = 'DOY'
+# plotname = "Eugene, OR, surface slope: "+str(slope_deg)+orient+ "Elevation dependency"
+# labels = ('Ra','Rso')
+# colors = ('r--','r', 'b--','b','g--','g','k--','k','m--','m','y--','y')
+# labloc = ("upper left","lower center", "upper center","lower left")
+#
+# fig1, ax1 = plt.subplots(figsize=(7, 5))
+# i = 0
+# for elev in elevation_array:
+#     result = run_radiation.run_radiation(latitude_deg, slope_deg, aspect_deg, elev, albedo, turbidity, temperature, rhumidity)
+#     # plot_results.plot_results(result[0], result[1], fig1, ax1, ymax, xname, yname, plotname, labels[0], colors[0])  # 1h
+#     plot_results.plot_results(result[0], result[2], fig1, ax1, ymax, xname, yname, plotname, str(elev), colors[i],labloc[3],ymin) # 1h
+#     # colors = ('b--','b')
+#     # plot_results.plot_results(result[0],result[3],result[4], fig1, ax1, ymax, xname, yname, plotname, labels, colors[i]) # 3h
+#     # colors = ('k--','k')
+#     # plot_results.plot_results(result[0],result[5],result[6], fig1, ax1, ymax, xname, yname, plotname, labels, colors[i]) # 24h
+#     i+=1
+# plt.show()
+#
+# albedo_array = [0.01, 0.05, 0.1, 0.25, 0.5, 0.99]
+# slope_deg = 90.0
+# ymax = 350
+# ymin = -20
+# yname = 'Rso, [W/m^2]'
+# xname = 'DOY'
+# plotname = "Eugene, OR, surface slope: "+str(slope_deg)+orient+ "Albedo dependency"
+# labels = ('Ra','Rso')
+# colors = ('r--','r', 'b--','b','g--','g','k--','k','m--','m','y--','y')
+# labloc = ("upper left","lower center", "upper center","lower left")
+#
+# fig1, ax1 = plt.subplots(figsize=(7, 5))
+# i = 0
+# for albedo in albedo_array:
+#     result = run_radiation.run_radiation(latitude_deg, slope_deg, aspect_deg, elevation, albedo, turbidity, temperature, rhumidity)
+#     # plot_results.plot_results(result[0], result[1], fig1, ax1, ymax, xname, yname, plotname, labels[0], colors[0])  # 1h
+#     plot_results.plot_results(result[0], result[2], fig1, ax1, ymax, xname, yname, plotname, str(albedo), colors[i],labloc[3],ymin) # 1h
+#     # colors = ('b--','b')
+#     # plot_results.plot_results(result[0],result[3],result[4], fig1, ax1, ymax, xname, yname, plotname, labels, colors[i]) # 3h
+#     # colors = ('k--','k')
+#     # plot_results.plot_results(result[0],result[5],result[6], fig1, ax1, ymax, xname, yname, plotname, labels, colors[i]) # 24h
+#     i+=1
+# plt.show()
+#
+# turbidity_array = [0.01, 0.05, 0.1, 0.25, 0.5, 1.0]
+# slope_deg = 90.0
+# ymax = 350
+# ymin = -20
+# yname = 'Rso, [W/m^2]'
+# xname = 'DOY'
+# plotname = "Eugene, OR, surface slope: "+str(slope_deg)+orient+ "Albedo dependency"
+# labels = ('Ra','Rso')
+# colors = ('r--','r', 'b--','b','g--','g','k--','k','m--','m','y--','y')
+# labloc = ("upper left","lower center", "upper center","lower left")
+#
+# fig1, ax1 = plt.subplots(figsize=(7, 5))
+# i = 0
+# for turbidity in turbidity_array:
+#     result = run_radiation.run_radiation(latitude_deg, slope_deg, aspect_deg, elevation, albedo, turbidity, temperature, rhumidity)
+#     # plot_results.plot_results(result[0], result[1], fig1, ax1, ymax, xname, yname, plotname, labels[0], colors[0])  # 1h
+#     plot_results.plot_results(result[0], result[2], fig1, ax1, ymax, xname, yname, plotname, str(turbidity), colors[i],labloc[3],ymin) # 1h
+#     # colors = ('b--','b')
+#     # plot_results.plot_results(result[0],result[3],result[4], fig1, ax1, ymax, xname, yname, plotname, labels, colors[i]) # 3h
+#     # colors = ('k--','k')
+#     # plot_results.plot_results(result[0],result[5],result[6], fig1, ax1, ymax, xname, yname, plotname, labels, colors[i]) # 24h
+#     i+=1
+# plt.show()
+
+# elevation_array = [-100.0, -20.0,0.0, 20.0, 100.0,400.0, 600.0, 800.0, 1000.0, 1800.0, 5000.0, 8800.0]
+# slope_deg = 90.0
+# ymax = 350
+# ymin = -20
+# yname = 'Rso, [W/m^2]'
+# xname = 'DOY'
+# plotname = "Eugene, OR, surface slope: "+str(slope_deg)+orient+ "Elevation (corrected (0.75+0.00002*elevation) from ASCE-EWRI) dependency"
+# labels = ('Ra','Rso')
+# colors = ('r--','r', 'b--','b','g--','g','k--','k','m--','m','y--','y')
+# labloc = ("upper left","lower center", "upper center","lower left")
+#
+# fig1, ax1 = plt.subplots(figsize=(7, 5))
+# i = 0
+# for elev in elevation_array:
+#     result = run_radiation.run_radiation(latitude_deg, slope_deg, aspect_deg, elev, albedo, turbidity, temperature, rhumidity)
+#     corr_result = []
+#     for r in result[2]:
+#         corr_result.append(r*(0.75+0.00002*elev))
+#     # plot_results.plot_results(result[0], result[1], fig1, ax1, ymax, xname, yname, plotname, labels[0], colors[0])  # 1h
+#     plot_results.plot_results(result[0], corr_result, fig1, ax1, ymax, xname, yname, plotname, str(elev), colors[i],labloc[3],ymin) # 1h
+#     # colors = ('b--','b')
+#     # plot_results.plot_results(result[0],result[3],result[4], fig1, ax1, ymax, xname, yname, plotname, labels, colors[i]) # 3h
+#     # colors = ('k--','k')
+#     # plot_results.plot_results(result[0],result[5],result[6], fig1, ax1, ymax, xname, yname, plotname, labels, colors[i]) # 24h
+#     i+=1
+# plt.show()
+
+aspect_array = [0.0, 90.0, 180] # S, EW, N
+slope_deg = 45.0
+ymax = 350
 ymin = -20
 yname = 'Rso, [W/m^2]'
 xname = 'DOY'
-plotname = "Eugene, OR, surface slope: "+str(slope_deg)+orient+ "Rhumidity dependency"
+plotname = "Eugene, OR, surface slope: "+str(slope_deg)+orient+ "Aspect dependency"
 labels = ('Ra','Rso')
 colors = ('r--','r', 'b--','b','g--','g','k--','k','m--','m','y--','y')
 labloc = ("upper left","lower center", "upper center","lower left")
 
 fig1, ax1 = plt.subplots(figsize=(7, 5))
 i = 0
-for elev in elevation_array:
-    result = run_radiation.run_radiation(latitude_deg, slope_deg, aspect_deg, elev, albedo, turbidity, temperature, rhumidity)
+for aspect in aspect_array:
+    result = run_radiation.run_radiation(latitude_deg, slope_deg, aspect, elevation, albedo, turbidity, temperature, rhumidity)
     # plot_results.plot_results(result[0], result[1], fig1, ax1, ymax, xname, yname, plotname, labels[0], colors[0])  # 1h
-    plot_results.plot_results(result[0], result[2], fig1, ax1, ymax, xname, yname, plotname, str(elev), colors[i],labloc[1],ymin) # 1h
+    plot_results.plot_results(result[0], result[2], fig1, ax1, ymax, xname, yname, plotname, str(aspect), colors[i],labloc[3],ymin) # 1h
     # colors = ('b--','b')
     # plot_results.plot_results(result[0],result[3],result[4], fig1, ax1, ymax, xname, yname, plotname, labels, colors[i]) # 3h
     # colors = ('k--','k')
